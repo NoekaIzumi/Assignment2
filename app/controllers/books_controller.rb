4517a7e6db58e3b822884fff2178787e10af4506
NoekaIzumi <noeka-izumi@outlook.jp>
2023-07-30 17:40:27 +0000

    [Pause]3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,7 @@ class BooksController < ApplicationController
 
  def create
    @book = Book.new(book_params)
-   if @book.save
-     redirect to '/new'
-    else
-      render :index
-    end
+
  end
 
   def index
@@ -17,9 +13,18 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+
   end
 
   def book_params
